735a9e692521f6d46e5eff467a694b86e08fd341
Timothy-py <adeyeyetimothy33@gmail.com>
2020-04-08 15:41:06 +0100

    initial django project setup

---
+++ stack_overfill_app/config/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for stack_overfill_app project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stack_overfill_app.settings')
+
+application = get_asgi_application()
