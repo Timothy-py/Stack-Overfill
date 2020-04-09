d4791dabd3390f7e41b8d36238489ae952742328
Timothy-py <adeyeyetimothy33@gmail.com>
2020-04-09 01:11:33 +0100

    created the question model

--- stack_overfill_app/core_app/models.py
+++ stack_overfill_app/core_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
+from django.urls.base import reverse
 
 # Create your models here.
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=140)
+    question = models.TextField()
+    # django recommends that that you never reference django.contrib.auth.models.User directly
+    # but using either settings.AUTH_USER_MODEL or django.contrib.auth.get_user_mode()
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)   # automatically set d field 2 now wen d obj is first created
+                                                        # as opposed to auto_now which is for updates.
+
+    def __str__(self):
+        return self.title
+
+    # a method to redirect to the detail page of a question immediately it's created
+    def get_absolute_url(self):
+        return reverse('core_app:question_detail', kwargs={'pk': self.id})
+
+    def can_accept_answers(self, user):     # ***
+        return user == self.user
