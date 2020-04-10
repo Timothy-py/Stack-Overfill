05378ffc846d48448279962498822ac63d7672e4
Timothy-py <adeyeyetimothy33@gmail.com>
2020-04-11 00:21:50 +0100

    configured route for question detail page

--- stack-overfill-app/core_app/urls.py
+++ stack-overfill-app/core_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AskQuestionView
+from .views import AskQuestionView, QuestionDetailView
 
 app_name = 'core_app'
 
 urlpatterns = [
-    path('ask', AskQuestionView.as_view(), name='ask_question')
+    path('ask', AskQuestionView.as_view(), name='ask_question'),
+    path('question/<int:pk>', QuestionDetailView.as_view(), name='question_detail'),
 ]
