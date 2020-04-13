ddaf046b042fac445e4dd0b4ca890add77bfdd9f
Timothy-py <adeyeyetimothy33@gmail.com>
2020-04-13 18:00:14 +0100

    developed user registration functionality

--- stack-overfill-app/user_app/views.py
+++ stack-overfill-app/user_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+
 
 # Create your views here.
+
+class RegisterView(CreateView):
+    template_name = 'user_app/register.html'
+    form_class = UserCreationForm
