e80f3d60c1e936d43560c021500aa00e98ef61fa
Timothy-py <adeyeyetimothy33@gmail.com>
2020-04-15 00:32:07 +0100

    finishing touches | implements a success url for user registration

--- stack-overfill-app/user_app/views.py
+++ stack-overfill-app/user_app/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
+from django.shortcuts import reverse
 
 
 # Create your views here.
@@ -7,3 +8,6 @@ from django.views.generic import CreateView
 class RegisterView(CreateView):
     template_name = 'register.html'
     form_class = UserCreationForm
+
+    def get_success_url(self):
+        return reverse('user_app:login')
